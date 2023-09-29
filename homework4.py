ab038fb40816450ad946a20303f9ddd06b57f040
Nightmee <pr1zetyou@gmail.com>
2023-09-29 15:54:03 +0300

    home5

--- homework4.py
+++ homework4.py
@@ -9,3 +9,10 @@ my_tuple = (6, 5, 4, 3, 2)
 my_list.append(my_dictionery)
 my_list.append(my_tuple)
 print(my_list)
+
+def array_diff(arr1, arr2):
+    return [x for x in arr1 if x not in arr2]
+
+result_func = array_diff([1,2,2,2,3,],[2])
+
+print(result_func)
